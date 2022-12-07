e11cb085ed85f0d751809385025d611dd5704a42
Teona Piranishvili <teona.piranishvili1@gmail.com>
2022-12-07 22:32:45 +0400

    feat: add filter

--- src/components/CompletedTodos.js
+++ src/components/CompletedTodos.js
@@ -4,33 +4,33 @@ import image1 from "../images/Vector1.png";
 import "../index.css";
 
 const CompletedTodos = ({ listItems, deleteItem, updateItem }) => {
-  const completedTodos = listItems.find((x) => x.completed == true);
+  const completedTodos = listItems.filter((x) => x.completed == true);
 
   return (
     <div className="mt-8">
-      <div className="flex justify-between mt-2">
-        <p>{completedTodos?.item}</p>
-        <div className="flex  ">
-          <button
-            style={{
-              backgroundColor: completedTodos?.completed ? "#20eeb0" : "white",
-            }}
-            onClick={() =>
-              updateItem(completedTodos?._id, completedTodos?.completed)
-            }
-            className="border rounded-full mr-1 w-[50%]"
-          >
-            <img className="m-auto" src={image1} />
-          </button>
-          <button
-            onClick={() => {
-              deleteItem(completedTodos?._id);
-            }}
-          >
-            <img className="h-[70%]" src={Vector}></img>
-          </button>
+      {completedTodos.map((item) => (
+        <div className="flex justify-between mt-2">
+          <p>{item.item}</p>
+          <div className="flex  ">
+            <button
+              style={{
+                backgroundColor: item.completed ? "#20eeb0" : "white",
+              }}
+              onClick={() => updateItem(item._id, item.completed)}
+              className="border rounded-full mr-1 w-[50%]"
+            >
+              <img className="m-auto" src={image1} />
+            </button>
+            <button
+              onClick={() => {
+                deleteItem(item._id);
+              }}
+            >
+              <img className="h-[70%]" src={Vector}></img>
+            </button>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
